a1358de26140f3eccc28de3f54f5f877f5bb96dd
dgurczynski <dominikgurczynski@gmail.com>
2026-03-31 18:30:53 +0200

    Update Pong.h

--- src/Pong.h
+++ src/Pong.h
@@ -1,110 +1,127 @@
 #pragma once
-#include <Arduino.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <Arduino.h>
 
 extern Adafruit_SSD1306 display;
 
+//Klasa obsługująca logikę gre
 class Pong {
-private:
-  float b_x, b_vx, b_y, b_vy;
-  int p1_y, p2_y;
-  int p1_score, p2_score;
-  int buzzer, potentiometer;
-  const float speed = 2.915;
+  private:
+    float b_x, b_vx, b_y, b_vy;
+    int p1_y, p2_y;
+    int p1_score, p2_score;
+    int buzzer, potentiometer;
+    const float speed = 2.915;
 
-  void reset_ball(int dir) {
-    b_x = 63;
-    b_y = (p1_score + p2_score) % 2 == 0 ? 16 : 47;
+    //Funkcja przywracająca piłkę na pozycję startową
+    void reset_ball(int dir) {
+        b_x = 63;
+        b_y = (p1_score + p2_score) % 2 == 0 ? 16 : 47;
 
-    b_vx = -dir * 2.5;
-    b_vy = 1.5;
-  }
-
-public:
-  void init(int pot, int buz) {
-    p1_score = 0;
-    p2_score = 0;
-    buzzer = buz;
-    potentiometer = pot;
-    reset_ball(1);
-  }
-
-  void update() {
-    p1_y = map(analogRead(potentiometer), 0, 4095, 1, 47);
-
-    float bot_speed = 1.8;
-    if (p2_y + 8 < b_y) { p2_y += bot_speed; }
-    else if (p2_y + 8 > b_y) { p2_y -= bot_speed; }
-
-    p2_y = constrain(p2_y, 1, 47);
-
-    float ball_how_far = 0;
-    float bounce_angle = 0;
-    float direction = 0;
-
-    if (b_x >= 121.0 && b_x <= 124.0 && b_y >= p2_y && b_y <= p2_y + 16) { 
-      ball_how_far = (b_y - (p2_y + 8)) / 8; 
-      direction = -1.0; 
-      b_x = 120.0;
-    }
-    else if (b_x >= 4.0 && b_x <= 7.0 && b_y >= p1_y && b_y <= p1_y + 16) { 
-      ball_how_far = (b_y - (p1_y + 8)) / 8; 
-      direction = 1.0;
-      b_x = 8.0;
+        b_vx = -dir * 2.5;
+        b_vy = 1.5;
     }
 
-    if (direction != 0) {
-      bounce_angle = ball_how_far * (M_PI / 3);
-      b_vx = direction * speed * cos(bounce_angle);
-      b_vy = speed * sin(bounce_angle);
-      tone(buzzer, 3000, 10);
+  public:
+    void init(int pot, int buz) {
+        p1_score = 0;
+        p2_score = 0;
+        buzzer = buz;
+        potentiometer = pot;
+        reset_ball(1);
     }
 
-    if (b_y > 62 || b_y < 1) { b_vy *= -1.0; tone(buzzer, 2000, 10); }
-  
-    b_x += b_vx;
-    b_y += b_vy;
+    //Główna logika gry
+    void update() {
+        p1_y = map(analogRead(potentiometer), 0, 4095, 1, 47);
 
-    if (b_x > 127) { 
-      p1_score++; 
-      tone(buzzer, 250, 150);
-      if(!is_game_over()) { reset_ball(-1); delay(1000); };
+        float bot_speed = 1.8;
+        if (p2_y + 8 < b_y) {
+            p2_y += bot_speed;
+        } else if (p2_y + 8 > b_y) {
+            p2_y -= bot_speed;
+        }
+
+        p2_y = constrain(p2_y, 1, 47);
+
+        float ball_how_far = 0;
+        float bounce_angle = 0;
+        float direction = 0;
+
+        if (b_x >= 121.0 && b_x <= 124.0 && b_y >= p2_y && b_y <= p2_y + 16) {
+            ball_how_far = (b_y - (p2_y + 8)) / 8;
+            direction = -1.0;
+            b_x = 120.0;
+        } else if (b_x >= 4.0 && b_x <= 7.0 && b_y >= p1_y && b_y <= p1_y + 16) {
+            ball_how_far = (b_y - (p1_y + 8)) / 8;
+            direction = 1.0;
+            b_x = 8.0;
+        }
+
+        if (direction != 0) {
+            bounce_angle = ball_how_far * (M_PI / 3);
+            b_vx = direction * speed * cos(bounce_angle);
+            b_vy = speed * sin(bounce_angle);
+            tone(buzzer, 3000, 10);
+        }
+
+        if (b_y > 62 || b_y < 1) {
+            b_vy *= -1.0;
+            tone(buzzer, 2000, 10);
+        }
+
+        b_x += b_vx;
+        b_y += b_vy;
+
+        if (b_x > 127) {
+            p1_score++;
+            tone(buzzer, 250, 150);
+            if (!is_game_over()) {
+                reset_ball(-1);
+                delay(1000);
+            };
+        } else if (b_x < 0) {
+            p2_score++;
+            tone(buzzer, 250, 150);
+            if (!is_game_over()) {
+                reset_ball(1);
+                delay(1000);
+            }
+        }
     }
-    else if (b_x < 0) { 
-      p2_score++;
-      tone(buzzer, 250, 150);
-      if(!is_game_over()) { reset_ball(1); delay(1000); }
+
+    bool is_game_over() { return (p1_score >= 11 || p2_score >= 11); }
+
+    //Funkcja renderująca grafikę ekranu OLED
+    void draw() {
+        display.clearDisplay();
+        display.drawFastHLine(0, 0, 128, WHITE);
+        display.drawFastHLine(0, 63, 128, WHITE);
+        display.fillRect(4, p1_y, 3, 16, WHITE);
+        display.fillRect(121, p2_y, 3, 16, WHITE);
+        display.fillCircle(b_x, b_y, 2, WHITE);
+
+        for (int i = 0; i < 64; i += 3) {
+            display.drawPixel(63, i, WHITE);
+        }
+
+        display.setTextColor(WHITE);
+        display.setTextSize(1);
+        display.setCursor(45, 5);
+        display.print(p1_score);
+        display.setCursor(78, 5);
+        display.print(p2_score);
+
+        if (is_game_over()) {
+            display.setCursor(25, 31);
+            if (p1_score == 11) {
+                display.print("PLAYER 1 WON!");
+            } else if (p2_score == 11) {
+                display.print("PLAYER 2 WON!");
+            }
+        }
+
+        display.display();
     }
-  }
-
-  bool is_game_over() {
-    return (p1_score >= 11 || p2_score >= 11);
-  }
-
-  void draw() {
-    display.clearDisplay();
-    display.drawFastHLine(0, 0, 128, WHITE);
-    display.drawFastHLine(0, 63, 128, WHITE);
-    display.fillRect(4, p1_y, 3, 16, WHITE);
-    display.fillRect(121, p2_y, 3, 16, WHITE);
-    display.fillCircle(b_x, b_y, 2, WHITE);
-
-    for (int i = 0; i < 64; i += 3) { display.drawPixel(63, i, WHITE); }
-
-    display.setTextColor(WHITE);
-    display.setTextSize(1);
-    display.setCursor(45, 5);
-    display.print(p1_score);
-    display.setCursor(78, 5);
-    display.print(p2_score);
-
-    if(is_game_over()) {
-      display.setCursor(25, 31);
-      if (p1_score == 11) { display.print("PLAYER 1 WON!"); }
-      else if (p2_score == 11) { display.print("PLAYER 2 WON!"); }
-    }
-
-    display.display();
-  }
 };
